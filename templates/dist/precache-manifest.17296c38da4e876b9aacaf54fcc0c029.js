c7e66d33e9f3556b411f1ebc7e5cd5c704ba47d7
403459169@qq.com <you@example.com>
2019-10-04 19:33:00 +0800

    chg react-10

---
+++ templates/dist/precache-manifest.17296c38da4e876b9aacaf54fcc0c029.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5dd5f8eedc5ff1dd67ba9871a63a68",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d9df8e6b5b8b2536500",
+    "url": "./static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "ae9b5e81d3a346f4610f",
+    "url": "./static/css/main.2d653c6c.chunk.css"
+  },
+  {
+    "revision": "1d9df8e6b5b8b2536500",
+    "url": "./static/js/2.f57cdb90.chunk.js"
+  },
+  {
+    "revision": "ae9b5e81d3a346f4610f",
+    "url": "./static/js/main.ce834cfd.chunk.js"
+  },
+  {
+    "revision": "822cdb67ddfcd67def68",
+    "url": "./static/js/runtime-main.d55cd90d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
